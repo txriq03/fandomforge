55b1d086e640abb4cc54b617174df86c0ec003d2
Tariq <118397532+txriq03@users.noreply.github.com>
2025-06-22 18:24:37 +0100

    :fire: remove flex and commented code

--- Components/MovieGrid.tsx
+++ Components/MovieGrid.tsx
@@ -37,11 +37,6 @@ const MovieGrid = () => {
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
       {data.results.map((movie: any) => (
         <div key={movie.id} className="flex flex-col items-center">
-          {/* <img
-            src={`${imageBaseUrl}${movie.poster_path}`}
-            alt={movie.title}
-            className="w-full rounded-lg shadow-md"
-          /> */}
           <SteamCard url={`${imageBaseUrl}${movie.poster_path}`} />
         </div>
       ))}
--- Components/SteamCard.tsx
+++ Components/SteamCard.tsx
@@ -1,7 +1,7 @@
 const SteamCard = ({ url }: { url?: string }) => {
   return (
-    <div className="w-full flex perspective-[1000px] perspective-origin-top h-[max-content] ">
-      <div className="group relative flex overflow-hidden origin-[100%_0] shadow-[0px 10px 39px 10px rgba(62, 66, 66, 0.22)] rounded-[2px] transition-[transform] duration-[0.3s] ease-in-out hover:rotate-x-[10deg]">
+    <div className="w-full  perspective-[1000px] perspective-origin-top h-[max-content] ">
+      <div className="group relative  overflow-hidden origin-[100%_0] shadow-[0px 10px 39px 10px rgba(62, 66, 66, 0.22)] rounded-[2px] transition-[transform] duration-[0.3s] ease-in-out hover:rotate-x-[10deg]">
         <img src={url} className="w-full" />
         <div className="absolute content-[''] top-0 left-0 z-1  origin-top-right w-[200%] h-[65%] opacity-[0.5] transition-all duration-[0.3s] ease-in-out bg-[linear-gradient(35deg,rgba(0,0,0,0.1)_0%,rgba(0,0,0,0.07)_51.5%,rgba(255,255,255,0.15)_54%,rgba(255,255,255,0.15)_100%)] group-hover:h-[120%] group-hover:opacity-70" />
       </div>
