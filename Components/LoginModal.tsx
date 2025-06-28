4812d2cef33395f62f4db5cbb2b0162eb5cf3ec2
Tariq <118397532+txriq03@users.noreply.github.com>
2025-06-28 07:52:58 +0100

    :bug: fix image not covering given space properly

--- Components/LoginModal.tsx
+++ Components/LoginModal.tsx
@@ -1,34 +1,62 @@
 "use client";
 import { useUIContext } from "@/providers/UIContext";
 import {
-  Image,
+  Button,
+  Input,
   Modal,
   ModalBody,
   ModalContent,
+  ModalFooter,
   ModalHeader,
 } from "@heroui/react";
-import React, { useContext } from "react";
+import { AtSign, Lock, Mail, User2 } from "lucide-react";
+import Image from "next/image";
+import React, { useContext, useState } from "react";
 
 const LoginModal = () => {
   const { isLoginOpen, onLoginOpenChange } = useUIContext();
+  const [showSignUp, toggleShowSignUp] = useState(false);
 
   return (
-    <Modal isOpen={isLoginOpen} onOpenChange={onLoginOpenChange} size="3xl">
+    <Modal
+      isOpen={isLoginOpen}
+      onOpenChange={onLoginOpenChange}
+      size="2xl"
+      className="font-main"
+    >
       <ModalContent>
         {(onClose) => (
           <div className="flex">
-            <div className="flex-1">
+            <div className="flex-1 h-[400px] hidden sm:block w-full relative">
+              <Image
+                fill
+                alt="login image"
+                src="/abstract.jpg"
+                className="object-cover h-full w-full "
+              />
+            </div>
+            <div className="flex-1 ">
               <ModalHeader className="text-primary text-2xl font-bold font-heading">
-                Sign in
+                {showSignUp ? "Sign up" : "Sign in"}
               </ModalHeader>
-              <LoginForm />
+              {showSignUp ? <SignupForm /> : <LoginForm />}
+              <ModalFooter className="flex flex-row items-center gap-1 text-sm justify-start pt-0">
+                {" "}
+                {showSignUp
+                  ? "Already have an account?"
+                  : "Don't have an account?"}
+                <Button
+                  variant="light"
+                  size="sm"
+                  className="text-sm"
+                  color="primary"
+                  onPress={() => toggleShowSignUp(!showSignUp)}
+                >
+                  {" "}
+                  {showSignUp ? "Sign in!" : "Sign up!"}
+                </Button>
+              </ModalFooter>
             </div>
-            <div className="flex-1 hidden sm:block">
-              <Image src="/abstract.jpg" radius="none" />
-            </div>
-            {/* <ModalHeader className="text-primary text-base sm:text-2xl">
-              Sign in
-            </ModalHeader> */}
           </div>
         )}
       </ModalContent>
@@ -36,8 +64,55 @@ const LoginModal = () => {
   );
 };
 
+const SignupForm = () => {
+  return (
+    <ModalBody>
+      <Input
+        placeholder="Username"
+        startContent={<User2 size={18} className="text-primary/50" />}
+      />
+      <Input
+        placeholder="Email"
+        type="email"
+        startContent={<AtSign size={18} className="text-primary/50" />}
+      />
+      <Input
+        placeholder="Password"
+        type="password"
+        startContent={<Lock size={18} className="text-primary/50" />}
+      />
+
+      <Input
+        placeholder="Confirm Password"
+        type="password"
+        startContent={<Lock size={18} className="text-primary/50" />}
+      />
+      <Button fullWidth color="primary" variant="shadow" size="lg">
+        Login
+      </Button>
+    </ModalBody>
+  );
+};
+
 const LoginForm = () => {
-  return <ModalBody>Test</ModalBody>;
+  return (
+    <ModalBody>
+      <Input
+        placeholder="Email"
+        type="email"
+        startContent={<AtSign size={18} className="text-primary/50" />}
+      />
+
+      <Input
+        placeholder="Password"
+        type="password"
+        startContent={<Lock size={18} className="text-primary/50" />}
+      />
+      <Button fullWidth color="primary" variant="shadow" size="lg">
+        Login
+      </Button>
+    </ModalBody>
+  );
 };
 
 export default LoginModal;
