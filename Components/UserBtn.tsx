b6946ab6a99e4f5e8126588a7a1c58ffb64a8a6d
Tariq <118397532+txriq03@users.noreply.github.com>
2025-07-03 16:01:25 +0100

    :sparkles: create Profiles type

--- Components/UserBtn.tsx
+++ Components/UserBtn.tsx
@@ -1,16 +1,17 @@
 "use client";
 
+import Profile from "@/types/profile";
 import { Avatar, Button } from "@heroui/react";
 import { ChevronDown } from "lucide-react";
 
-const UserBtn = ({ profile }: any) => {
+const UserBtn = ({ profile }: { profile: Profile }) => {
   return (
     <div className="h-full flex gap-2 rounded-full bg-black/5 sm:pr-1 items-center">
       <Avatar showFallback src={"/default_pfp.png"} />
       <div className=" flex-col hidden sm:flex">
         <p className="text-sm">{profile.username}</p>
         <p className="text-[0.8rem] text-black/50">
-          Level <span className="font-semibold">1</span>
+          Level <span className="font-semibold">{profile.level}</span>
         </p>
       </div>
       <Button
---
+++ types/profile.ts
@@ -0,0 +1,9 @@
+type Profile = {
+  id: string;
+  is_online: boolean | null;
+  level: number;
+  profile_pic: string | null;
+  username: string | null;
+};
+
+export default Profile;
