2df81c782a10de96101b4aab2d04e08f97a23cc7
Tariq <118397532+txriq03@users.noreply.github.com>
2025-07-07 18:23:00 +0100

    :bug: remove invalid getProfile function in queryFn

--- Components/navigation/Topbar.tsx
+++ Components/navigation/Topbar.tsx
@@ -1,25 +1,8 @@
 "use client";
 import SearchBox from "../SearchBox";
-import { Button } from "@heroui/react";
-import { Bell, LogIn, LogOut } from "lucide-react";
-import { useUIContext } from "@/providers/UIContext";
-import UserBtn from "../SidebarFooter";
-import { useQuery } from "@tanstack/react-query";
-import { devLog } from "@/lib/utils";
-import { getProfile } from "@/lib/supabase/client-actions";
 import Notifications from "../Notifications";
 
 const Topbar = ({ user }: any) => {
-  const { onLoginOpen } = useUIContext();
-
-  const { data: profile } = useQuery({
-    queryKey: ["profile"],
-    queryFn: getProfile,
-  });
-
-  devLog.log("Profile", profile);
-  devLog.log("User", user);
-
   return (
     <div className=" w-full  lg:px-4 lg:pt-2">
       <div className="bg-sidebar py-2 px-2 lg:rounded-xl flex justify-between gap-4 outline-1 outline-indigo-500/15">
