be2debfa4083e2660b6d99bd4f2cd345d93c7a5c
Tariq <118397532+txriq03@users.noreply.github.com>
2025-07-20 01:37:53 +0100

    :lipstick: add extra padding at the bottom for review page

--- app/[media_type]/[id]/reviews/page.tsx
+++ app/[media_type]/[id]/reviews/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 const ReviewsPage = () => {
   return (
-    <Padding className="pt-2 pb-10">
+    <Padding className="pt-2 pb-20">
       <ReviewMessageBox />
       {/* <Divider className="mt-[30px] mb-[15px] bg-primary/25" /> */}
       <p className="pt-10 pb-2 text-sm sm:text-lg text-foreground/60 font-light">
