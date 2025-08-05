c2264150da3d3e9f8df5b0f86eb77227be8e9cc1
Tariq <118397532+txriq03@users.noreply.github.com>
2025-08-06 00:58:03 +0100

    :lipstick: add dotted background to footer

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   const githubLink = "https://github.com/txriq03";
   const portfolio = "https://tariqbrown.dev";
   return (
-    <footer className="w-full bg-black/20 text-center py-5 text-sm mt-10 flex flex-col gap-1 items-center">
+    <footer className="w-full bg-black/20 text-center py-5 text-sm mt-10 flex flex-col gap-1 items-center relative">
       <p>Developed with ❤️ and ☕</p>
       <div className="inline">
         <span className="text-white/75">by</span>{" "}
@@ -13,6 +13,17 @@ const Footer = () => {
           Tariq
         </Link>
       </div>
+
+      <div
+        className="absolute inset-0 z-0 bg-black/10"
+        style={{
+          backgroundImage: `
+        radial-gradient(circle, rgba(255, 255, 255, 0.2) 1.5px, transparent 1.5px)
+      `,
+          backgroundSize: "30px 30px",
+          backgroundPosition: "0 0",
+        }}
+      />
     </footer>
   );
 };
