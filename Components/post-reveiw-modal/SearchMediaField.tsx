aa1b08dd2b23256e52fbd453d42b034675643be0
txriq03 <taqbro@gmail.com>
2025-08-10 23:44:41 +0100

    :sparkles: add mediaType state for MediaTypeSelect

--- Components/post-reveiw-modal/SearchMediaField.tsx
+++ Components/post-reveiw-modal/SearchMediaField.tsx
@@ -1,20 +1,39 @@
 import { cn } from "@/lib/utils";
+import { MediaType } from "@/types/trending";
 import { Autocomplete, AutocompleteItem } from "@heroui/autocomplete";
 import { Select, SelectItem } from "@heroui/select";
-import React from "react";
+import React, { Dispatch, useState } from "react";
 
 const SearchMediaField = () => {
+  const [mediaType, setMediaType] = useState<MediaType>("movie");
   return (
     <div className="flex gap-2">
       <SearchMediaAutoComplete className="flex-2" />
-      <MediaTypeSelect className="flex-1" />
+      <MediaTypeSelect
+        className="flex-1"
+        mediaType={mediaType}
+        setMediaType={setMediaType}
+      />
     </div>
   );
 };
 
-const MediaTypeSelect = ({ className }: { className?: string }) => {
+const MediaTypeSelect = ({
+  className,
+  mediaType = "movie",
+  setMediaType,
+}: {
+  className?: string;
+  mediaType: MediaType;
+  setMediaType: Dispatch<React.SetStateAction<MediaType>>;
+}) => {
   return (
-    <Select className={cn(className)} defaultSelectedKeys={["movie"]}>
+    <Select
+      className={cn(className)}
+      defaultSelectedKeys={["movie"]}
+      value={mediaType}
+      onChange={(e) => setMediaType(e.target.value as MediaType)}
+    >
       <SelectItem key="movie">Movie</SelectItem>
       <SelectItem key="tv">TV</SelectItem>
     </Select>
